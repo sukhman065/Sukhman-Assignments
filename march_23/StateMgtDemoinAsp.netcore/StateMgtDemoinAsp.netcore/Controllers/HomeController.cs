85d5017239d131fd44cf446d865e79f390526d8c
Sukhmanpreet Kaur <2237483.cse.cec@cgc.edu.in>
2026-03-23 14:58:28 +0530

    codepush

---
+++ march_23/StateMgtDemoinAsp.netcore/StateMgtDemoinAsp.netcore/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
+using StateMgtDemoinAsp.netcore.Models;
+using System.Diagnostics;
+
+namespace StateMgtDemoinAsp.netcore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        private int a = 0;
+        [HttpPost]
+        public IActionResult SetA()
+        {
+            a = 10;
+            ViewBag.AValue = "A has been set to 10";
+            return View("Index");
+        }
+        [HttpPost]
+        public IActionResult GetA()
+        {
+            ViewBag.AValue = $"A is currently :{a}";
+            return View("Index");
+        }
+        public IActionResult Index()
+        {
+            TempData["myKey"] = "Data from Index method";
+            return View();
+        }
+
+        public IActionResult Index2()
+       {
+            //HttpContext.Session.Remove("myKey");
+            //TempData.Peek("myKey");
+            //ViewBag.MyKey = TempData["myKey"];
+            //TempData.Keep("myKey");
+            return View();
+        }
+        public IActionResult Index3()
+        {
+            TempData.Peek("myKey");
+            ViewBag.MyKey = TempData.Peek("myKey");
+            return View();
+        }
+        public IActionResult Index4()
+        {
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ march_23/StateMgtDemoinAsp.netcore/StateMgtDemoinAsp.netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StateMgtDemoinAsp.netcore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

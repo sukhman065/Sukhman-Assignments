168bd8e9167d36557fe6d7889ede21dee3d880ed
Sukhmanpreet Kaur <2237483.cse.cec@cgc.edu.in>
2026-03-28 10:06:07 +0530

    Pushing code

---
+++ march_25/WebApiInAsp.netcoreMvcDemo/WebApiInAsp.netcoreMvcDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiInAsp.netcoreMvcDemo.Models;
+
+namespace WebApiInAsp.netcoreMvcDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<EmpContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("constring")));
+
+            builder.Services.AddScoped<IEmployee, EmployeeService>();
+
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
---
+++ march_27/WebApiInAsp.netcoreMvcDemo/WebApiInAsp.netcoreMvcDemo/Models/EmpContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiInAsp.netcoreMvcDemo.Models
+{
+    public class EmpContext:DbContext
+    {
+        public EmpContext(DbContextOptions dbContextOptions) :
+            base(dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> employees { set; get; }
+
+    }
+}
